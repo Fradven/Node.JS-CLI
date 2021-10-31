48eb9b5d922f74146c4660bd61bcd4c289e4c687
Fradven <frederic.vanovermeire@gmail.com>
2021-10-31 12:39:48 +0100

    working code for testing

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-console.log("Hello, Node.JS!");
+const [,, ...args] = process.argv
+const axios = require('axios').default;
+const {getCode} = require('country-list');
+
+const currentYear = new Date().getFullYear()
+const country = getCode(...args)
+
+const getHolidays = (year, code) => {
+    axios.get(`https://date.nager.at/api/v2/PublicHolidays/${year}/${code}`).then(
+        response => response.data.forEach(element => {
+            console.log(`${element.date}: ${element.name}`)
+        })
+    ).catch(
+        error => console.log(error)
+    )
+}
+
+getHolidays(currentYear, country)
